e555197e08375f53add7edc37bd9eb750e7f4e4e
prLorence <paulorencereyes@gmail.com>
2021-05-12 11:03:44 +0800

    i learned the concept of callbacks

--- filter_and_map.js
+++ filter_and_map.js
@@ -1,15 +1,22 @@
 // I LEARNED HOW TO USE MAP METHODS
 
 function cleanNames(strings) {
-  const cleaned = strings.map(name => {
-      return name.trim();
-  });
-  return cleaned;
+  const cleaned = strings.map((name) => {
+    return name.trim()
+  })
+  return cleaned
 }
 
-// I learned filter methods 
+// I learned filter methods
 
 const validUserNames = (words) => {
-  const fltrdNames = words.filter(word => word.length < 10);
-  return fltrdNames;
- };
+  const fltrdNames = words.filter((word) => word.length < 10)
+  return fltrdNames
+}
+
+// This i how learned some/every
+
+const allEvens = (num) => {
+  const isEven = num.every((digit) => digit % 2 === 0)
+  return isEven
+}
