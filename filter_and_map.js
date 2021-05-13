0937f80f7ab0e549820723297aadf4ae30da7f91
prLorence <paulorencereyes@gmail.com>
2021-05-13 10:32:21 +0800

    I learned the new concepts like higer order functions etc.

--- filter_and_map.js
+++ filter_and_map.js
@@ -27,30 +27,60 @@ const callTenTimes = function (func) {
   for (let i = 0; i < 10; i++) {
     func()
   }
-};
+}
 
 function coinFlip(coin) {
-
-  if (coin = Math.floor(Math.random() * 10) <= 5) {
-    console.log('Tails');
+  if ((coin = Math.floor(Math.random() * 10) <= 5)) {
+    console.log("Tails")
   } else {
-    console.log('Heads')
+    console.log("Heads")
   }
-
 }
 
 callTenTimes(coinFlip)
 
 // Returning functions could be useful
 
-function canIAfford (price) {
-  return function(myMoney) {
+function canIAfford(price) {
+  return function (myMoney) {
     if (price > myMoney) {
-      return "I can't afford it";
+      return "I can't afford it"
     } else {
       return "I can buy it now"
     }
   }
 }
 
+// Function methods are one way to create your own methods if necessary (objects + functions = function methods)
 
+const square = {
+  area(num) {
+    return num * num
+  },
+  perimeter(num) {
+    return num * 4
+  },
+}
+
+// I should look into 'this' more closely, this is just an simple illustration what it can do inside an object.
+
+const hen = {
+  name: "Helen",
+  eggCount: 0,
+  layAnEgg() {
+    this.eggCount++
+    return "EGG"
+  },
+}
+
+// Try and catch will definitely help me in the future
+
+function tryAndCatch(val) {
+  try {
+    console.log(val.toUpperCase.repeat(5))
+  } catch {
+    console.log(
+      "You inputted a non-string value, this only works in a string format"
+    )
+  }
+}
